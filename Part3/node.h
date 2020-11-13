ab01c459bdc51bc35afa5dc394cdedc7321ae1f9
mathieugillin <68657159+mathieugillin@users.noreply.github.com>
2020-11-13 13:29:42 +0000

    Update node.h

--- Part3/node.h
+++ Part3/node.h
@@ -6,38 +6,75 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-// Do not add any #include statements here.  If you have a convincing need for adding a different `#include` please post in the forum on KEATS.
-
 // TODO your code for the Node class goes here:
 // (Note the 'NodeIterator' class will report lots of errors until Node exists
 
+template<typename T>
+class Node{
+public:
+  T data;
+  Node<T> *previous;
+  Node<T> *next;
+  
+
+  Node(T dataInput)
+      : data(dataInput),next(nullptr),previous(nullptr){
+  }
+};
 
 
 
 
 template<typename T>
 class NodeIterator {
-  
-private:
-    
-    Node<T>* current;
-    
 public:
-    
+
+  Node<T>* current;
+
+
 
     NodeIterator(Node<T>* currentIn)
-        : current(currentIn) {        
+        : current(currentIn) {
     }
 
-    T & operator*() {
+
+
+     void operator++(){
+      current = current->next;
+    }
+
+
+    NodeIterator()
+        : current(nullptr) {
+    }
+
+  bool operator==(const NodeIterator<T> &other) const{
+      return (current == other.current);
+    }
+
+
+
+
+    T & operator*() const{
         return current->data;
     }
 
-    // TODO: complete the code for NodeIterator here
-        
+
+
+   
+
+    bool operator!=(const NodeIterator<T> &other) const{
+      return (current != other.current);
+    }
+
     
+
+
+
+
 };
 
 // do not edit below this line
 
 #endif
+
