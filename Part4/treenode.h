ebb57cf58a11339b4fad53cf60d84f04273a107d
mathieugillin <68657159+mathieugillin@users.noreply.github.com>
2020-11-13 15:20:20 +0000

    Update treenode.h

--- Part4/treenode.h
+++ Part4/treenode.h
@@ -14,7 +14,37 @@ using std::unique_ptr;
 using std::pair;
 
 // TODO your code for the TreeNode class goes here:
+template<typename T>
+class TreeNode{
+	
+public:
+	T data;
+	unique_ptr<TreeNode<T>> leftChild;
+	unique_ptr<TreeNode<T>> rightChild;
+	TreeNode<T> * parent;
+	
+	TreeNode(const T value):data(value),parent(nullptr){
+		
+	}
 
+	void setLeftChild(TreeNode *child){
+		leftChild.reset(child);
+		leftChild->parent=this;	
+	}
+	
+	void setRightChild(TreeNode *child){
+		rightChild.reset(child);
+		rightChild->parent=this;	
+	}
+	
+	void write(ostream & o)const{
+    if(leftChild){
+      leftChild->write(o);}
+    o<< " " << data << " " ;
+    if(rightChild){
+      rightChild->write(o);}
+    }	
+};
 // do not edit below this line
 
 #endif
